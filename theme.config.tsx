ebd8dbc7ac0d24417a35f71867b6334c475f31e8
chenyakun002 <chenyakun002@lianjia.com>
2024-11-23 22:39:43 +0800

    change

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useEffect } from 'react'
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
@@ -13,6 +14,26 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Nextra Docs Template',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 网站名称'
+    }
+  },
+  head: () => {
+    return (
+      <>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              // 这里放置您的 JavaScript 代码
+              console.log('网站加载完成！');
+              // 其他代码...
+            `
+          }}
+        />
+      </>
+    )
+  }
 }
 
 export default config
